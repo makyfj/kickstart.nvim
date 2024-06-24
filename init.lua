a96f77d13bf0a15bcbfe24580208aa990b9ad144
Franklin <franklinjaradev@gmail.com>
2024-06-24 06:47:58 -0400

    feat: commands for sg.lua

--- init.lua
+++ init.lua
@@ -669,7 +669,9 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
       },
     },
   },
