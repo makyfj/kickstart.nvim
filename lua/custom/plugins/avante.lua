f30682252b42e5c0c2cca394f1a1444ce651e137
Franklin <franklinjaradev@gmail.com>
2024-08-18 13:07:10 -0400

    updating avante config

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -2,7 +2,7 @@ return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   opts = {
-    provider = 'claude',
+    provider = 'groq', -- "claude" or "openai" or "azure" or "deepseek" or "groq"
     windows = {
       width = 40,
     },
