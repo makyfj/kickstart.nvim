6d126e24145cf62dc97334e6022286f0fc4858d3
Franklin <franklinjaradev@gmail.com>
2024-08-27 15:28:03 -0400

    adding vendors to avante

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -2,9 +2,67 @@ return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   opts = {
-    provider = 'claude',
+    provider = 'groq',
     windows = {
       width = 40,
+    },
+    vendors = {
+      ---@type AvanteProvider
+      groq = {
+        endpoint = 'https://api.groq.com/openai/v1/chat/completions',
+        model = 'llama-3.1-70b-versatile',
+        api_key_name = 'GROQ_API_KEY',
+        --- this function below will be used to parse in cURL arguments.
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint,
+            headers = {
+              ['Accept'] = 'application/json',
+              ['Content-Type'] = 'application/json',
+              ['Authorization'] = 'Bearer ' .. os.getenv(opts.api_key_name),
+            },
+            body = {
+              model = opts.model,
+              messages = require('avante.providers').openai.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              temperature = 0,
+              max_tokens = 2048,
+              stream = true, -- this will be set by default.
+            },
+          }
+        end,
+        -- The below function is used if the vendors has specific SSE spec that is not claude or openai.
+        parse_response_data = function(data_stream, event_state, opts)
+          require('avante.providers').openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+      ---@type AvanteProvider
+      cerebras = {
+        endpoint = 'https://api.cerebras.ai/v1/chat/completions',
+        model = 'llama3.1-70b',
+        api_key_name = 'CEREBRAS_API_KEY',
+        --- this function below will be used to parse in cURL arguments.
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint,
+            headers = {
+              ['Accept'] = 'application/json',
+              ['Content-Type'] = 'application/json',
+              ['Authorization'] = 'Bearer ' .. os.getenv(opts.api_key_name),
+            },
+            body = {
+              model = opts.model,
+              messages = require('avante.providers').openai.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              temperature = 0,
+              max_tokens = 8192,
+              stream = true, -- this will be set by default.
+            },
+          }
+        end,
+        -- The below function is used if the vendors has specific SSE spec that is not claude or openai.
+        parse_response_data = function(data_stream, event_state, opts)
+          require('avante.providers').openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
     },
   },
   build = 'make',
